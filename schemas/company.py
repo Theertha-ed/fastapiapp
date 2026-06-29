79cda0e0647ded7c64b7a346197e0cc58f33889e
Theertha-ed <edtheertha@gmail.com>
2026-06-29 09:10:37 +0530

    test load

--- schemas/company.py
+++ schemas/company.py
@@ -1,24 +1,24 @@
 from pydantic import BaseModel
 from typing import Optional
 from .job import JobResponse
+
+
 class CompanyBase(BaseModel):
-    name:str
-    email:str
-    phone:str
-    
+    name: str
+    email: str
+    phone: str
+
 class CompanyCreate(CompanyBase):
     pass
 
 class CompanyUpdate(CompanyBase):
-    name:Optional[str]=None
-    email:Optional[str]=None
-    phone:Optional[str]=None
-    
+    name: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+
 class CompanyResponse(CompanyBase):
     id:int
-    jobs:list[JobResponse]
-    
+    jobs: list[JobResponse]
+
     class Config:
-        from_attributes=True   
-    
-    
+        from_attributes = True
