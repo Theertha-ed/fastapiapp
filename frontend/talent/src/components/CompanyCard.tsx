410ed447a9eda73512eae4d190b0f39dc2882b7e
Theertha-ed <edtheertha@gmail.com>
2026-06-30 12:05:43 +0530

    test company

---
+++ frontend/talent/src/components/CompanyCard.tsx
@@ -0,0 +1,30 @@
+import { getCompanies } from "../Services/CompanyService";
+import { useEffect, useState } from "react";
+import type { Company } from "../types/company";
+
+
+function CompanyCard() {
+  const [companies, setCompanies] = useState<Company[]>([]);
+  async function fetchCompanies() {
+    const companies = await getCompanies();
+    setCompanies(companies);
+  }
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
+  return (
+      <div>
+       {companies.map((company) => (
+        <div key={company.id}>
+          <h1>{company.name}</h1>
+          <p>{company.email}</p>
+          <p>{company.phone}</p>
+          <p>{company.location}</p>
+          <hr></hr>
+        </div>
+      ))}
+      </div>
+  )
+}
+export default CompanyCard
